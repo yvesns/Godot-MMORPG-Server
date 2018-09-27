9c166447f16d36f47ca9bda38f89f78a6ed771c3
yvesns <yves.ns2@gmail.com>
2018-09-26 22:02:29 -0300

    Trying to use prepared statements

--- DatabaseManager.gd
+++ DatabaseManager.gd
@@ -1,22 +1,28 @@
 extends Node
 
-const SQLite = preload("res://lib/gdsqlite.gdns");
+var SQLite
+#var SQLite = preload("res://lib/gdsqlite.gdns")
 var db
 var dbFile = "res://database.sql"
+
+enum BindType { DOUBLE, INT, TEXT }
 
 func _ready():
 	pass
 
 func init_database():
+	SQLite = load("res://lib/gdsqlite.gdns")
 	var result = null
 	
 	db = SQLite.new()
 	
-	if (!db.open_db(dbFile)):
+	if (!db.open(dbFile)):
 		print("Failed to open database.")
 		return
 		
-	print(db.query(DatabaseQueries.get_create_user_table_query()))
+	print(db.simple_query(DatabaseQueries.create_user_table()))
+	
+	print(db.query(DatabaseQueries.insert_user()), ["test", str("test".hash())], [TEXT, TEXT])
 	
 func has_user(user):
 	pass
--- DatabaseQueries.gd
+++ DatabaseQueries.gd
@@ -2,13 +2,16 @@ extends Node
 
 var user_table = "GameUser"
 
-func get_create_user_table_query():
-	var query = ""
+func create_user_table():
+	var query
 	
 	query = "CREATE TABLE IF NOT EXISTS " + user_table + " (";
-	query += "id integer PRIMARY KEY,";
+	query += "id integer PRIMARY KEY AUTOINCREMENT,";
 	query += "login text NOT NULL,";
-	query += "password_hash integer NOT NULL,";
+	query += "password_hash integer NOT NULL";
 	query += ");";
 	
 	return query
+	
+func insert_user(): 
+	return "INSERT INTO " + user_table + "(login, password_hash) VALUES(?,?);"
